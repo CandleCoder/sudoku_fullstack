863cc80cbffa502ca0378c1902589fe5694acb18
Manikant Upadhyay <manikantupadhyay@gmail.com>
2021-03-28 11:02:49 +0530

    MongoDB Connection Established

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
-const mongoose = require('./src/backEnd/utils/db/mongoConnector')
+require('./src/backEnd/utils/db/')
 
+// Send in Response if the App is Running in Default Route
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Console If the App is Running on Port or Not
 app.listen(port, () => {
   console.log(`Sudoku Backend listening at http://localhost:${port}`)
 })
