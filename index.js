3bee8797d0be975daa51d98ac34f76d919f478fa
Manikant Upadhyay <manikantupadhyay@gmail.com>
2021-03-29 08:21:08 +0530

    Restructured the Backend App

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-require("./src/backEnd/utils/db/mongoConnector");
-const masterRoute = require("./src/backEnd/routes/masterRoute");
+const cors = require("cors");
+const logger = require("morgan");
+require("./src/utils/db/mongoConnector");
+const masterRoute = require("./src/routes/masterRoute");
 
 // Send in Response if the App is Running in Default Route
 app.get("/", (req, res) => {
@@ -14,6 +16,9 @@ app.use(express.json());
 
 // Indicating the Versioning of the API
 app.use("/v1", masterRoute);
+
+app.use(logger('dev'));
+app.use(cors());
 
 // Console If the App is Running on Port or Not
 app.listen(port, () => {
